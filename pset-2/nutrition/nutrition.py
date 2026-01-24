ec14bc232c8da7e0eb62869b4a627f5836485c80
deividas2026 <deividasaldo@protonmail.com>
2026-01-24 23:11:55 +0200

    add pset-2/nutrition/nutrition.py

---
+++ pset-2/nutrition/nutrition.py
@@ -0,0 +1,27 @@
+fruit_table = {
+	"apple": 130,
+	"avocado": 50,
+	"banana": 110,
+	"cantaloupe": 50,
+	"grapefruit": 60,
+	"grapes": 90,
+	"honeydew": 50,
+	"kiwifruit": 90,
+	"lemon": 15,
+	"lime": 20,
+	"nectarine": 60,
+	"orange": 80,
+	"peach": 60,
+	"pear": 100,
+	"pineapple": 50,
+	"plums": 70,
+	"strawberries": 50,
+	"sweet cherries": 100,
+	"tangerine": 50,
+	"watermelon": 80,
+}
+
+fruit = input("Item: ").lower()
+if fruit in fruit_table:
+	print(f"Calories: {fruit_table[fruit]}")
+
