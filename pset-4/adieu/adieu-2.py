968d3745bfa2c1cc5e3931987d6df57c30414db2
deividas2026 <deividasaldo@protonmail.com>
2026-01-26 00:18:21 +0200

    add second version of adieu.py that uses inflect library

---
+++ pset-4/adieu/adieu-2.py
@@ -0,0 +1,14 @@
+import inflect
+p = inflect.engine()
+names = []
+
+while True:
+	try:
+		name = input("Name: ")
+	except EOFError:
+		print()
+		break
+	else:
+		names.append(name)
+
+print("Adieu, adieu, to " + p.join(names))
